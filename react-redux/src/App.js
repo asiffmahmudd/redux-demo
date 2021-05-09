dbde7b3fd9eded0faeefa17ca4140085ead6a7a0
Asif Mahmud <asifmahmud3472@gmail.com>
2021-05-09 20:36:51 +0600

    react redux setup

---
+++ react-redux/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CakeContainer from './components/CakeContainer/CakeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer></CakeContainer>
+    </div>
+  );
+}
+
+export default App;
